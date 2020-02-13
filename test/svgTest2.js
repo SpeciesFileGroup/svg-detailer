78117219a4aa3209c54bc8d269bd0e66b44eb37d
jrflood <jrflood@illinois.edu>
2020-02-13 17:49:44 -0600

    add circle test

---
+++ test/svgTest2.js
@@ -0,0 +1,61 @@
+const { Builder, By, Key, until} = require('selenium-webdriver');
+const {expect} = require('chai');
+describe('Circle creation', () => {
+  const driver = new Builder().forBrowser('firefox').build();
+  // let mode = driver.findElement(By.id, 'mode');
+  driver.manage().setTimeouts({implicit: 40000});
+  const actions = driver.actions();
+
+  it('Should create an element with id g1 and type circle', async () => {
+    await driver.get('http://localhost:8081/');
+    await driver.findElement(By.id('image_file')).sendKeys('/Users/jrichardflood/RubymineProjects/svg-detailer/test/images/testImage.jpg');
+    let element, type, id, xoff, yoff, zoom, transform, cx, cy, r;
+    try {
+      element = await driver.findElement(By.id('container'));
+      xoff = parseInt(await element.getAttribute('offsetLeft').then(function (x) {return x}));
+      yoff = parseInt(await element.getAttribute('offsetTop').then(function (x) {return x}));
+      // console.log('xoff: ' + xoff + ' | yoff: ' + yoff);
+      element = await driver.findElement(By.id('xlt'));
+      transform = await element.getAttribute('transform');
+      // console.log(transform, typeof transform);
+      zoom = transform.split('(');
+      zoom = zoom[2].split(')');
+      // console.log(zoom[0]);
+      zoom = parseFloat(zoom[0]);  //((transform.toString()).split('(')[2]).split(')')[0]);
+      // console.log('xoff: ' + xoff + ' | yoff: ' + yoff + ' | zoom: ' + zoom);
+    }
+    catch (event) {
+      console.log(event);
+    }
+    try {
+      cx = 300 + xoff;
+      cy = 300 + yoff;
+      r = 200;
+      await driver.findElement(By.css('#b_circle')).click();
+      await actions.move({x: cx, y: cy, duration: 100}).press();
+      await actions.move({x: cx + r, y: cy, duration: 1000});
+      await actions.release().perform();
+      element = await driver.findElement(By.id('g1'));
+      id = await element.getAttribute('id').then(function (x) {return x});
+      type = await element.getAttribute('type').then(function (x) {return x});
+      // console.log('id: ' + id + ' | type: ' + type);
+      element = await driver.findElement(By.css('circle'));
+      cx = await element.getAttribute('cx').then(function (x) {return x});
+      cy = await element.getAttribute('cy').then(function (x) {return x});
+      r = await element.getAttribute('r').then(function (x) {return x});
+      // console.log('cx: ' + cx + ' | cy: ' + cy);
+    }
+    catch (event) {
+      console.log(event);
+    }
+    finally {
+      expect(type).to.equal('circle', 'type');
+      expect(id).to.equal('g1', 'id');
+      expect(cx).to.equal((300/zoom).toString(), 'cx');
+      expect(cy).to.equal((300/zoom).toString(), 'cy');
+      expect(r).to.equal((200/zoom).toString(), 'r')
+    }
+    let mode = await driver.findElement(By.id('b_move')).click();
+    driver.quit();
+  })
+});
