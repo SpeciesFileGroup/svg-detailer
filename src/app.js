97f1680ece6044ba14981ee03c95ae1e3aa6b274
jrflood <jrflood@illinois.edu>
2020-02-14 13:58:25 -0600

    Refactor tests, names.  Add polyline (in progress)

--- src/app.js
+++ src/app.js
@@ -1,44 +1,25 @@
 import SVGDraw from './svg-detailer.js'
 
-const svgContainer = document.getElementById('container')
-const imageInput = document.getElementById('image_file')
-const fakeInput = document.getElementById('auto_file')
+const svgContainer = document.getElementById('container');
+const imageInput = document.getElementById('image_file');
 var svg;
 
 imageInput.addEventListener('change', (event) => {
   getImage(event);
 });
-fakeInput.addEventListener('click', (event) => {
-  fakeImage(event);
-});
 
 function getImage(event) {
   let files = event.target.files;
-  let svg_element = svgContainer.children[0]
+  let svg_element = svgContainer.children[0];
   if(svg_element) {svg_element.parentNode.removeChild(svg_element)}  // remove any previously created svg element
-  let svg_menu = document.getElementById("svgMenu")
+  let svg_menu = document.getElementById("svgMenu");
   if(svg_menu) {svg_menu.parentNode.removeChild(svg_menu)}      // force removal of any created peer menu element
   if (FileReader && files && files.length) {
     var fileReader = new FileReader();
     fileReader.onload = (image) => {
-      svgContainer.attributes["data-image"].value = fileReader.result
+      svgContainer.attributes["data-image"].value = fileReader.result;
       svg = new SVGDraw(svgContainer)
-    }
+    };
     fileReader.readAsDataURL(files[0])
   }
-}
-function fakeImage(event) {
-  // let files = event.target.files;
-  console.log('faker');
-  document.getElementById('choose').innerText = "did it work?"
-  let svg_container = document.getElementById("container");
-  let svg_element = svg_container.children[0];
-  if(svg_element) {svg_element.parentNode.removeChild(svg_element)}  // remove any previously created svg element
-  let svg_menu = document.getElementById("svgMenu");
-  if(svg_menu) {svg_menu.parentNode.removeChild(svg_menu)}      // force removal of any created peer menu element
-
-  // svg_container.attributes["data-image"].src = 'http://localhost:8081/tests/images/testImage.jpg';
-  console.log('render?')
-  svg_container.attributes["data-image"].value = 'file:///Users/jrichardflood/RubymineProjects/svg-detailer/tests/images/testImage.jpg';
-  svg = new SVGDraw(svg_container);
 }
