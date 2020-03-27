7889a96911c641d120f668b3bdf6ae458afe43ac
jrflood <jrflood@illinois.edu>
2020-03-26 22:17:04 -0500

    attempt at default image plus file input

--- src/demo.js
+++ src/demo.js
@@ -1,3 +1,27 @@
 import SVGDraw from './svg-detailer'
 
-var svgDraw = new SVGDraw(document.getElementById("container"));
+// var svgDraw = new SVGDraw(document.getElementById("container"));
+
+const svgContainer = document.getElementById('container');
+const imageInput = document.getElementById('image_file');
+var svg;
+
+imageInput.addEventListener('change', (event) => {
+  getImage(event);
+});
+
+function getImage(event) {
+  let files = event.target.files;
+  let svg_element = svgContainer.children[0];
+  if(svg_element) {svg_element.parentNode.removeChild(svg_element)}  // remove any previously created svg element
+  let svg_menu = document.getElementById("svgMenu");
+  if(svg_menu) {svg_menu.parentNode.removeChild(svg_menu)}      // force removal of any created peer menu element
+  if (FileReader && files && files.length) {
+    var fileReader = new FileReader();
+    fileReader.onload = (image) => {
+      svgContainer.attributes["data-image"].value = fileReader.result;
+      svg = new SVGDraw(svgContainer)
+    };
+    fileReader.readAsDataURL(files[0])
+  }
+}
